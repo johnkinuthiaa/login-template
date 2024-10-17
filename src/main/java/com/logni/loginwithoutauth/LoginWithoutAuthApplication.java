6e995310d0e8cf31110fd3f573861ad9fbb2a7fa
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-17 23:00:46 +0300

    initial commit

---
+++ src/main/java/com/logni/loginwithoutauth/LoginWithoutAuthApplication.java
@@ -0,0 +1,13 @@
+package com.logni.loginwithoutauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithoutAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWithoutAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logni/loginwithoutauth/LoginWithoutAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logni.loginwithoutauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithoutAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
